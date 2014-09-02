c95e8cdeffd3396a37079e1d9a0cd87fdae7ee19
Chris Holden <ceholden@gmail.com>
2014-09-02 13:19:38 -0400

    Added test for MOD09GA mask

---
+++ tests/test_prep_MODIS.py
@@ -0,0 +1,64 @@
+""" Test pre-processing workflow
+"""
+import os
+import sys
+import unittest
+
+import numpy as np
+from osgeo import gdal
+
+gdal.UseExceptions()
+gdal.AllRegister()
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(here, '../'))
+
+from nrtwarn.prep import prep_MODIS
+
+
+class PreprocessTest(unittest.TestCase):
+    """ Tests for `prep_MODIS.py`
+
+    Tests are run against pre-existing MATLAB code results
+    """
+
+    def setUp(self):
+        """ Load in test data """
+        mod09ga = 'MOD09G/MOD09GA.A2000055.h10v08.005.2006268014216.hdf'
+        ds = gdal.Open(os.path.join(here, mod09ga), gdal.GA_ReadOnly)
+        state_sds = ds.GetSubDatasets()[1]
+        ds = None
+        self.state_ds = gdal.Open(state_sds[0], gdal.GA_ReadOnly)
+        self.state = self.state_ds.GetRasterBand(1).ReadAsArray()
+        self.state_ds = None
+
+    def testSetUp(self):
+        """ Test full sum of QA band """
+        total = 14906462501
+        self.assertEqual(self.state.sum(), total)
+
+    def testMaskSum(self):
+        """ Test total number of good pixels in mask result """
+        total = 1048607
+        mask = prep_MODIS.get_mask(self.state, dilate=7)
+        self.assertEqual(mask.sum(), total)
+
+    def testMaskLocations(self):
+        """ Test assortment of pixel values in mask """
+        px = [49, 349, 999, 998, 749, 1199]
+        py = [0, 98, 199, 399, 998, 0]
+
+        values = [0, 0, 1, 1, 1, 1]
+
+        mask = prep_MODIS.get_mask(self.state, dilate=7)
+
+        for _px, _py, _v in zip(px, py, values):
+            self.assertEqual(
+                mask[_py, _px], _v,
+                'Pixel x/y {px}/{py} is wrong ({m} vs. {t})'.format(
+                    px=_px, py=_py, m=mask[_py, _px], t=_v))
+
+
+if __name__ == '__main__':
+    unittest.main()
